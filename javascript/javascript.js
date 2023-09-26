7487c8b5fede6554999705791d2fb183fc5d31f5
Bjorn-23 <bjornandersson84@gmail.com>
2023-09-26 10:24:40 +0200

    i stort sett färdig

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,14 +1,16 @@
-function myFunction() {
-    var x = document.getElementById("menu-Bars");
-    if (x.className === "menu-bars") {
-      x.className += " responsive";
-    } else {
-      x.className = "menu-bars";
-    }
-}
+// For (.absolute-mb) mobile menu - not currently used
+
+// function myFunction() {
+//     var x = document.getElementById("menu-Bars");
+//     if (x.className === "menu-bars") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "menu-bars";
+//     }
+// }
 
 
-// for mobile menu to slide in
+// for mobile menu (#myNav) to slide in
 
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
