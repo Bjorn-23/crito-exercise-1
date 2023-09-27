b2b85f59944b3248bc9faf0e504a302c4fb87fcc
Bjorn-23 <bjornandersson84@gmail.com>
2023-09-27 16:56:48 +0200

    la till en del javascript Fredrik hittat på youtube för att skicka formdata till en annan sida.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -20,3 +20,11 @@ function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
 
+
+
+var element = document.getElementById('back-link');
+element.setAttribute('href', document.referrer);
+element.onclick = function() {
+  history.back();
+  return false;
+}
