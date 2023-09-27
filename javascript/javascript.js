4d47861b0e01cda56951d7b833ab486dd5dc4035
Bjorn-23 <bjornandersson84@gmail.com>
2023-09-27 17:12:25 +0200

    fixade lite på artikel-länkar

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -23,7 +23,9 @@ function closeNav() {
 
 
 var element = document.getElementById('back-link');
+
 element.setAttribute('href', document.referrer);
+
 element.onclick = function() {
   history.back();
   return false;
